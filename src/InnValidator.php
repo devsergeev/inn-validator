48650e0b8c040fa0aebf0264b51f764717ec3a68
LeTraceurSnork <le_traceur_snork@mail.ru>
2022-11-23 23:07:14 +0500

    fix: PHPDoc + typo fixed (lenght -> length)

--- src/InnValidator.php
+++ src/InnValidator.php
@@ -4,51 +4,75 @@ namespace devsergeev\validators;
 
 use InvalidArgumentException;
 
+/**
+ * Валидатор ИНН (Индивидуального номера налогонлательщика), российский аналог VAT
+ */
 class InnValidator
 {
-    public const CODE_INVALID_LENGHT   = 1;
+    public const CODE_INVALID_LENGTH   = 1;
     public const CODE_NOT_ONLY_DIGITS  = 2;
     public const CODE_INVALID_CHECKSUM = 3;
 
-    public static $messageInvalidLenght   = 'ИНН должен иметь длину 10 (физлицо) или 12 (юрлицо) символов';
+    public static $messageInvalidLength   = 'ИНН должен иметь длину 10 (физлицо) или 12 (юрлицо) символов';
     public static $messageOnlyDigits      = 'ИНН должен состоять только из цифр';
     public static $messageInvalidChecksum = 'ИНН недействителен (неверная контрольная сумма)';
 
+    /**
+     * Проверяет ИНН на валидность по последней (для Физлиц) или двум последним (для Юрлиц) символам
+     *
+     * @param string $inn
+     *
+     * @return bool
+     * @throws InvalidArgumentException
+     */
     public static function check(string $inn): bool
     {
         $innSymbols = str_split($inn);
-        $innLenght = mb_strlen($inn);
-        if ($innLenght === 10) {
-            self::checkInnSymbols($inn, $innLenght);
+        $innLength  = mb_strlen($inn);
+        if ($innLength === 10) {
+            self::checkInnSymbols($inn, $innLength);
             self::checkChecksum($innSymbols, 9);
-        } else if ($innLenght === 12) {
-            self::checkInnSymbols($inn, $innLenght);
+        } else if ($innLength === 12) {
+            self::checkInnSymbols($inn, $innLength);
             self::checkChecksum($innSymbols, 10);
             self::checkChecksum($innSymbols, 11);
         } else {
-            throw new InvalidArgumentException((string) self::$messageInvalidLenght, self::CODE_INVALID_LENGHT);
+            throw new InvalidArgumentException(self::$messageInvalidLength, self::CODE_INVALID_LENGTH);
         }
+
         return true;
     }
 
+    /**
+     * @param array $innSymbols
+     * @param int   $checkInnIndex
+     *
+     * @return void
+     */
     private static function checkChecksum(array $innSymbols, int $checkInnIndex): void
     {
-        $checksum = 0;
-        $offset = 11 - $checkInnIndex;
+        $checksum     = 0;
+        $offset       = 11 - $checkInnIndex;
         $coefficients = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
         for ($innIndex = 0; isset($coefficients[$innIndex + $offset]); $innIndex++) {
-            $checksum += (int) $innSymbols[$innIndex] * $coefficients[$innIndex + $offset];
+            $checksum += (int)$innSymbols[$innIndex] * $coefficients[$innIndex + $offset];
         }
         if ((int)$innSymbols[$checkInnIndex] !== $checksum % 11 % 10) {
-            throw new InvalidArgumentException((string) self::$messageInvalidChecksum, self::CODE_INVALID_CHECKSUM);
+            throw new InvalidArgumentException(self::$messageInvalidChecksum, self::CODE_INVALID_CHECKSUM);
         }
     }
 
-    private static function checkInnSymbols(string $inn, int $innLenght): void
+    /**
+     * @param string $inn
+     * @param int    $innLength
+     *
+     * @return void
+     */
+    private static function checkInnSymbols(string $inn, int $innLength): void
     {
-        for ($innIndex = 0; $innIndex < $innLenght; $innIndex++) {
+        for ($innIndex = 0; $innIndex < $innLength; $innIndex++) {
             if (!is_numeric($inn[$innIndex])) {
-                throw new InvalidArgumentException((string) self::$messageOnlyDigits, self::CODE_NOT_ONLY_DIGITS);
+                throw new InvalidArgumentException(self::$messageOnlyDigits, self::CODE_NOT_ONLY_DIGITS);
             }
         }
     }
